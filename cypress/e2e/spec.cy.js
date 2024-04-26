5a17f8b1a4a5a971c5cfd8bc1dd1a88b0017fcb7
Tommi <tommi.savolainen79@gmail.com>
2024-04-26 13:43:49 +0300

    testi ohjelma

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,15 @@
+describe('Blog app', () => {
+    beforeEach(function () {
+        cy.request('POST', 'http://localhost:3003/api/testing/reset');
+        const user = {
+            name: 'Tommi Savolainen',
+            username: 'tommi',
+            password: 'salasana',
+        };
+        cy.request('POST', 'http://localhost:3003/api/users/', user);
+        cy.visit('http://localhost:5173');
+    });
+    it('front page can be opened', () => {
+        cy.contains('Log in to application');
+    });
+});
